53a10204e5e456f3b273d6ad6882e4ca300d0fe2
Alessandro <alessandrohenriqueho@gmail.com>
2018-10-28 14:56:15 -0300

    Add CollectionForm component to the main app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import CollectionForm from './CollectionForm';
 
 class App extends Component {
   render() {
@@ -7,6 +8,7 @@ class App extends Component {
       <div>
         <Header />
         <div className="container">
+          <CollectionForm />
         </div>
       </div>
     );
