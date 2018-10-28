74c9785a4edb7b5feebac61f609c7a6b89994477
Alessandro <alessandrohenriqueho@gmail.com>
2018-10-28 14:51:52 -0300

    Change App.test to pass store

--- client/src/tests/components/App.test.js
+++ client/src/tests/components/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../../components/App';
+import Store from '../../store/configureStore';
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+
+const store = Store();
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const component = renderer.create(
+    <Provider store={store}>
+      <App />
+    </Provider>
+    );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
