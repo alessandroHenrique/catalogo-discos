195bf72d3a04be8ad25989eb1d8d2c10411c6d31
Alessandro <alessandrohenriqueho@gmail.com>
2018-10-28 13:21:52 -0300

    Add actions for the discs actionTypes

---
+++ client/src/actions/discs.js
@@ -0,0 +1,46 @@
+import DiscApi from '../api/discs';
+import {
+  ADD_DISC,
+  EDIT_DISC,
+  DELETE_DISC
+} from '../actionTypes/discs';
+
+export const addDiscSuccess = (disc) => ({
+  type: ADD_DISC,
+  disc
+});
+
+export const addDisc = (disc) => {
+  return (dispatch, getState) => {
+    return DiscApi.addDisc(disc).then((data) => {
+      return dispatch(addDiscSuccess(data));
+    });
+  };
+};
+
+export const updateDiscSuccess = (disc) => ({
+  type: EDIT_DISC,
+  disc
+});
+
+export const updateDisc = (disc) => {
+  return (dispatch, getState) => {
+    return DiscApi.updateDisc(disc).then((data) => {
+      return dispatch(updateDiscSuccess(data));
+    });
+  };
+};
+
+export const deleteDiscSuccess = (id, collectionId) => ({
+  type: DELETE_DISC,
+  id,
+  collectionId
+});
+
+export const deleteDisc = (id, disc_collection_id) => {
+  return (dispatch, getState) => {
+    return DiscApi.deleteDisc(id).then((id) => {
+      return dispatch(deleteDiscSuccess(id, disc_collection_id));
+    });
+  };
+};
