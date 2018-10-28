96cde4bf9195ea37f4e211ad77f52cf68666397d
Alessandro <alessandrohenriqueho@gmail.com>
2018-10-28 11:14:28 -0300

    Move App.js to the new components folder

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
