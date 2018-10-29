fe278e94c5c8e797a2e24fac62e1c147ac95b01b
Alessandro <alessandrohenriqueho@gmail.com>
2018-10-28 22:10:29 -0300

    Use router for the collections and discs endpoints

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
+const apiRouter = require('./routes/api');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', apiRouter);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
